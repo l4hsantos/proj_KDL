b854ab7046d2503ce9a612acba291a4f85cd0916
l4hsantos <lss52@discente.ifpe.edu.br>
2025-10-02 12:42:11 -0300

    Update server.js

--- server.js
+++ server.js
@@ -11,3 +11,49 @@ app.use(express.json());
 app.listen(port, () => {
     console.log(`Servidor em execução http://localhost:${port}`);
 });
+
+app.get('/pessoas', (req, res) => {
+    res.json(pessoas);
+});
+
+
+//listar todas as pessoas 
+app.get('/pessoas/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const pessoa = pessoas.find(p => p.id === id);
+    if (pessoa) {
+        res.json(pessoa);
+    } else {
+        res.status(404).json({ error: 'Pessoa não encontrada' });
+    }});
+
+app.post('/pessoas', (req, res) => {
+    const nome = req.body.nome;
+
+    const novaPessoa = {
+        id: pessoas.length + 1,
+        nome: nome }
+    pessoas.push(novaPessoa);
+    res.status(201).json(novaPessoa);
+}); 
+
+app.put('/pessoas/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const { nome } = req.body;
+    const pessoa = pessoas.find(i => i.id === id);
+    if (pessoa) {
+        pessoa.nome = nome;
+        res.json(pessoa);
+    } else {
+        res.status(404).json({ error: 'Pessoa não encontrada' });
+    }}); 
+
+    app.delete('/pessoas/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+        const index = pessoas.findIndex(i => i.id === id); 
+        if (index !== -1) {
+           const pessoaDeletada = pessoas.splice(index, 1);
+           res.json(pessoaDeletada[0]);
+        } else {
+            res.status(404).json({ error: 'Pessoa não encontrada' });
+        }});
